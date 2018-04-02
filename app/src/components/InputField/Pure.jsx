bfd79ef3195d701de732553d3469e15829ebf6f5
Leok Si <leoksi.koh@gmail.com>
2018-04-02 10:33:07 +0800

    Create inputfield pure component with styles

--- app/src/components/InputField/Pure.jsx
+++ app/src/components/InputField/Pure.jsx
@@ -3,37 +3,58 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const Input = styled.input`
-  width: 20%;
-  height: 32px;
+  ${({size}) => inputSize[size]}
   border-radius: 30px;
   padding: 0 10px;
   border: 1px solid #D54435;
   background-color: #D54435;
   color: #080E40;
-  font-size: 16px;
   &::-webkit-input-placeholder {
     color: #353F50;
   }
 `
 
+const inputSize = {
+  small: `
+    width: 150px;
+    height: 32px;
+    font-size: 12px;
+  `,
+  medium: `
+    width: 200px;
+    height: 36px;
+    font-size: 14px;
+  `,
+  large: `
+    width: 240px;
+    height: 40px;
+    font-size: 16px;
+  `
+}
+
 export default function InputField (props) {
-  const {placeholder, name, onChange, value} = props
+  const {size, placeholder, name, type, onChange, value} = props
   return (
     <Input
+      size={size}
       onChange={onChange}
       value={value}
-      type='text'
+      type={type}
       placeholder={placeholder}
       name={name} />
   )
 }
 
 InputField.defaultProps = {
+  size: 'medium',
+  type: 'text',
   onChange: () => null,
-  placeholder: 'Seach Location'
+  placeholder: 'Search Location'
 }
 
 InputField.propTypes = {
+  size: PropTypes.string,
+  type: PropTypes.string,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   disabled: PropTypes.bool,
   onChange: PropTypes.func.isRequired,
