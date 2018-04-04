9ce43d4d7a251bdf75cbd62610ffcaa68acdeef1
Leok Si <leoksi.koh@gmail.com>
2018-04-04 13:19:38 +0800

    remove unused styles and change applayout to functional component

--- app/src/components/Tabs/Pure.js
+++ app/src/components/Tabs/Pure.js
@@ -5,11 +5,10 @@ const NavBarTabs = props => {
   const {value, onChange} = props
   return (
     <Tabs
-      style={{width: '100%'}}
+      style={{width: '50%'}}
       value={value}
       onChange={onChange}
     >
-      <Tab label='HEAT MAP' value='HEATMAP' />
       <Tab label='BIKE USAGE' value='BIKEUSAGE' />
       <Tab label='WEATHER' value='WEATHER' />
     </Tabs>
