5e459391cc14f3db03036ae169632962f4ae8fe8
Leok Si <leoksi.koh@gmail.com>
2018-04-09 13:08:13 +0800

    fix z index on calendar picker div and map div

--- app/src/components/Heatmap/Pure.jsx
+++ app/src/components/Heatmap/Pure.jsx
@@ -16,15 +16,23 @@ const markerLabelStyle = {
 
 const Heatmap = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDZvxOHHY7Y6FPH1JwhgEE28YWSV7LHDV0&v=3.exp&libraries=visualization,geometry,drawing,places',
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyDZvxOHHY7Y6FPH1JwhgEE28YWSV7LHDV0&v=3.exp&libraries=visualization,geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `400px`, zIndex: `1` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
-  withGoogleMap)(props => {
-  const { isMarkerShown, currentMarker, toggleMarkerLabelVisibilityAction, hideMarkerLabelAction } = props
-  const handleMouseOver = e => toggleMarkerLabelVisibilityAction(e.Fa.target.title)
+  withGoogleMap
+)(props => {
+  const {
+    isMarkerShown,
+    currentMarker,
+    toggleMarkerLabelVisibilityAction,
+    hideMarkerLabelAction
+  } = props
+  const handleMouseOver = e =>
+    toggleMarkerLabelVisibilityAction(e.Fa.target.title)
   const getPoints = () => {
     return [
       new google.maps.LatLng(52.232, -0.2),
@@ -32,45 +40,40 @@ const Heatmap = compose(
       new google.maps.LatLng(52.225, -0.18),
       new google.maps.LatLng(52.222, -0.189),
       new google.maps.LatLng(52.222, -0.189),
-      new google.maps.LatLng(52.240, -0.189),
+      new google.maps.LatLng(52.24, -0.189),
       new google.maps.LatLng(52.235, -0.189),
-      new google.maps.LatLng(52.230, -0.189),
-      new google.maps.LatLng(52.230, -0.189)
+      new google.maps.LatLng(52.23, -0.189),
+      new google.maps.LatLng(52.23, -0.189)
     ]
   }
   return (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 52.232, lng: -0.233 }}>
+    <GoogleMap defaultZoom={13} defaultCenter={{ lat: 52.232, lng: -0.233 }}>
+      <HeatmapLayer data={getPoints()} options={{ radius: '80' }} />
 
-      <HeatmapLayer
-        data={getPoints()}
-        options={({ radius: '80' })} />
-
-      {isMarkerShown &&
-      <MarkerWithLabel
-        title='marker1'
-        position={{ lat: 52.232, lng: -0.2 }}
-        labelAnchor={new google.maps.Point(0, 100)}
-        labelStyle={markerLabelStyle}
-        onMouseOver={(e) => handleMouseOver(e)}
-        onMouseOut={() => hideMarkerLabelAction()}
-        labelVisible={currentMarker === 'marker1' ? true : false}>
-        <div>Craven Street Strand</div>
-      </MarkerWithLabel>
-      }
-      {isMarkerShown &&
-      <MarkerWithLabel
-        title='marker2'
-        position={{ lat: 52.232, lng: -0.182 }}
-        labelAnchor={new google.maps.Point(0, 100)}
-        labelStyle={markerLabelStyle}
-        onMouseOver={(e) => handleMouseOver(e)}
-        onMouseOut={() => hideMarkerLabelAction()}
-        labelVisible={currentMarker === 'marker2' ? true : false}>
-        <div>Westminster Street</div>
-      </MarkerWithLabel>
-      }
+      {isMarkerShown && (
+        <MarkerWithLabel
+          title="marker1"
+          position={{ lat: 52.232, lng: -0.2 }}
+          labelAnchor={new google.maps.Point(0, 100)}
+          labelStyle={markerLabelStyle}
+          onMouseOver={e => handleMouseOver(e)}
+          onMouseOut={() => hideMarkerLabelAction()}
+          labelVisible={currentMarker === 'marker1' ? true : false}>
+          <div>Craven Street Strand</div>
+        </MarkerWithLabel>
+      )}
+      {isMarkerShown && (
+        <MarkerWithLabel
+          title="marker2"
+          position={{ lat: 52.232, lng: -0.182 }}
+          labelAnchor={new google.maps.Point(0, 100)}
+          labelStyle={markerLabelStyle}
+          onMouseOver={e => handleMouseOver(e)}
+          onMouseOut={() => hideMarkerLabelAction()}
+          labelVisible={currentMarker === 'marker2' ? true : false}>
+          <div>Westminster Street</div>
+        </MarkerWithLabel>
+      )}
     </GoogleMap>
   )
 })
