cd7b4de18f1bb526331aaf1d48841b88d7f17d59
Leok Si <leoksi.koh@gmail.com>
2018-04-10 11:20:39 +0800

    fix the date string format so that redux store can reflect the right date

--- app/src/constants/index.js
+++ app/src/constants/index.js
@@ -1,10 +1,10 @@
 export const publicHolidaysLondon = {
-  'Early May Bank Holiday 2017': new Date('05 01 2017'),
-  'Spring Bank Holiday 2017': new Date('05 29 2017'),
-  'Summer Bank holiday 2017': new Date('08 28 2017'),
-  'Christmas Day 2017': new Date('12 25 2017'),
-  'Boxing Day 2017': new Date('12 26 2017'),
-  'New Year\'s Day 2018': new Date('01 01 2018'),
-  'Good Friday 2018': new Date('04 22 2018'),
-  'Easter Monday 2018': new Date('04 19 2018')
+  'Early May Bank Holiday 2017': new Date('2017-05-01'),
+  'Spring Bank Holiday 2017': new Date('2017-05-29'),
+  'Summer Bank holiday 2017': new Date('2017-08-28'),
+  'Christmas Day 2017': new Date('2017-12-25'),
+  'Boxing Day 2017': new Date('2017-12-26'),
+  'New Year\'s Day 2018': new Date('2018-01-01'),
+  'Good Friday 2018': new Date('2018-04-22'),
+  'Easter Monday 2018': new Date('2018-04-19')
 }
